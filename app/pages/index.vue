0b1795b926aa68ce6378643119cd64c87b25f0ec
HiroshiOyamada <h5y1m141+github@gmail.com>
2026-01-23 08:01:47 +0900

    Composablesのロジックを利用したページ表示ロジック実装

---
+++ app/pages/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+  const {count, increment, decrement, reset} = useCounter(0)
+</script>
+<template>
+   <div>
+     <h1>Counter: {{ count }}</h1>
+     <button @click="increment">+</button>
+     <button @click="decrement">-</button>
+     <button @click="reset">Reset</button>
+   </div>  
+</template>
